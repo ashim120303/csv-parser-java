e368436f531bcfc5facbfebf6d902c26c9c3dc72
ashim120303 <ashim030312@gmail.com>
2023-12-08 13:06:40 +0600

    Fix csv saving.

--- src/main/java/org/example/FileHandler.java
+++ src/main/java/org/example/FileHandler.java
@@ -9,13 +9,33 @@ public class FileHandler {
 
     public static void saveToFile(String data, String filePath) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
-            writer.write(data);
-            writer.newLine();
-            System.out.println("Сохранено: " + data);
+            // Разбиваем строку по запятой
+            String[] parts = data.split(",");
+
+            // Проверяем, что есть хотя бы две части
+            if (parts.length >= 2) {
+                // Записываем первую часть в первую колонку
+                writer.write(parts[0].trim());
+                writer.write(",");
+
+                // Записываем остальные части во вторую колонку
+                for (int i = 1; i < parts.length; i++) {
+                    writer.write(parts[i].trim());
+                    if (i < parts.length - 1) {
+                        writer.write(",");
+                    }
+                }
+
+                writer.newLine();
+                System.out.println("Сохранено: " + data);
+            } else {
+                System.out.println("Неверный формат данных: " + data);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
 
     public static void createFileIfNotExists(String filePath) {
         File file = new File(filePath);
